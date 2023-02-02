3177d4f72412b0aaee47f9b1827cdee3f9f13dc3
JavGuerra <javguerra@badared.com>
2023-02-02 13:16:26 +0100

    Primer commit

--- src/java_cp1_javier_guerra/MediaFormat.java
+++ src/java_cp1_javier_guerra/MediaFormat.java
@@ -12,24 +12,22 @@ public enum MediaFormat {
      * @see java_cp1_javier_guerra@ConsoleInput#getLongIntPosByRange getLongIntPosByRange
      */
     public static MediaFormat getMediaFormat(String type) {
-        MediaFormat mediaFormat = null;
+        MediaFormat mediaFormat;
         if (type.trim().equalsIgnoreCase("Analog")) {
             System.out.println("Formatos analógicos (1) Doble álbum, (2) LP, (3) Sencillo, (4) Cinta, (5) Cartucho");
             byte opt = getLongIntPosByRange("Elija un formato: ", 1L, 5L).byteValue();
-            switch (opt) {
-                case 1 -> mediaFormat = DOUBLE_ALBUM;
-                case 2 -> mediaFormat = LP;
-                case 3 -> mediaFormat = SINGLE;
-                case 4 -> mediaFormat = CASSETTE;
-                case 5 -> mediaFormat = CARTRIDGE;
-            }
+            mediaFormat = switch (opt) {
+                case 1 -> DOUBLE_ALBUM;
+                case 3 -> SINGLE;
+                case 4 -> CASSETTE;
+                case 5 -> CARTRIDGE;
+                default -> LP;
+            };
         } else if (type.trim().equalsIgnoreCase("Digital")) {
             System.out.println("Formatos digitales (1) CD, (2) Lista de reproducción");
             byte opt = getLongIntPosByRange("Elija un formato: ", 1L, 2L).byteValue();
-            switch (opt) {
-                case 1 -> mediaFormat = CD;
-                case 2 -> mediaFormat = PLAY_LIST;
-            }
+            if (opt == 1) mediaFormat = CD;
+            else mediaFormat = PLAY_LIST;
         } else {
             mediaFormat = PLAY_LIST;
         }
